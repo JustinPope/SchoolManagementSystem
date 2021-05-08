c4713b5b327ce6fd0877db35af91a737c9f57565
Justin <jbp3@sfu.ca>
2021-05-08 14:27:46 -0700

    Started working on a simple text GUI

--- src/SchoolManagementSystem/Main.java
+++ src/SchoolManagementSystem/Main.java
@@ -1,6 +1,7 @@
 package SchoolManagementSystem;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 /**
@@ -11,9 +12,9 @@ import java.util.ArrayList;
  */
 public class Main {
 
+	/**
 	public static void main(String[] args) {
 		
-		/* Test Functions
 		Teacher Kenneth = new Teacher("Kenneth Kim", 1, 300);
 		Teacher Lawson = new Teacher("Andy Lawson", 2, 250);
 		Teacher Selzer = new Teacher("Jessica Selzer", 3, 350);
@@ -47,7 +48,62 @@ public class Main {
 		System.out.println("SFU has earned: " + SFU.getTotalMoneyEarned());
 		System.out.println(Kat);
 		System.out.println(Selzer);
-		*/
+	}
+	*/
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int option;
+		String teachersName;
+		int counter = 0;
+		int otherCounter = 0;
+		int salary;
+		int year;
+		String studentsName;
+		String schoolName;
+		List<Teacher> Teachers = new ArrayList<>();
+		List<Student> Students = new ArrayList<>();
+		
+		System.out.println("What is your schools name?");
+		schoolName = in.next();
+		
+		while(true) {
+			System.out.println("Would you like to add a new teacher to your school? 1: Yes	2: No");
+			option = Integer.parseInt(in.next());
+			if(option == 1) {
+				System.out.println("What is the teachers name?");
+				teachersName = in.next();
+				System.out.println("what is the teachers salary?");
+				salary = Integer.parseInt(in.next());
+				Teacher temp = new Teacher(teachersName, counter++, salary);
+				Teachers.add(temp);
+			} else if(option == 2) {
+				while(true) {
+					System.out.println("Would you like to add a new student to your school? 1: Yes	2: No");
+					option = Integer.parseInt(in.next());
+					if(option == 1) {
+						System.out.println("What is the students name?");
+						studentsName = in.next();
+						System.out.println("what year is the student in?");
+						year = Integer.parseInt(in.next());
+						Student temp = new Student(studentsName, otherCounter++, year);
+						Students.add(temp);
+					} else if(option == 2) {
+						while(true) {
+							School tmap = new School(Teachers, Students);
+							System.out.println("Your school " + schoolName + " has been created!");
+							System.out.println("Would you like to view earnings? 1: Yes		2: No");
+							option = Integer.parseInt(in.next());
+							if(option == 1) {
+								System.out.println(schoolName + " has earned: " + tmap.getTotalMoneyEarned());
+							} else if(option == 2) {
+								break;
+							}
+						}
+					} 
+				}
+			} 
+		}
 	}
 
 }
